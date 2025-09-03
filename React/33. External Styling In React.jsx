c97bcc4fcaa1e318f1dae8d83e2bfddaeaebd266
Umair Sajawal <97627448+UmairSajawal@users.noreply.github.com>
2025-09-03 20:36:08 +0500

    Add files via upload

---
+++ React/33. External Styling In React.jsx
@@ -0,0 +1,162 @@
+// ******* External Styling In React *******
+
+/*
+App.jsx file code:
+import './css/style.css'
+function App() {
+
+    return (
+        <>
+        <h1 className="heading">External Styling In React</h1>
+        <div className="container">
+
+            <div className="user-card">
+            <div>
+                <img src="https://img.freepik.com/premium-vector/admin-clerk-with-laptop-icon_1076610-101107.jpg" alt="" />
+                </div>
+                <div>
+                <h4>Software Developer</h4>
+                <p>Umair Sajawal</p>
+                </div>
+            </div>
+
+            <div className="user-card">
+            <div>
+                <img src="https://img.freepik.com/premium-vector/admin-clerk-with-laptop-icon_1076610-101107.jpg" alt="" />
+            </div>
+            <div>
+                <h4>Software Developer</h4>
+                <p>Umair Sajawal</p>
+            </div>
+            </div>
+
+            <div className="user-card">
+            <div>
+                <img src="https://img.freepik.com/premium-vector/admin-clerk-with-laptop-icon_1076610-101107.jpg" alt="" />
+                </div>
+                <div>
+                <h4>Software Developer</h4>
+                <p>Umair Sajawal</p>
+                </div>
+            </div>
+
+            <div className="user-card">
+            <div>
+                <img src="https://img.freepik.com/premium-vector/admin-clerk-with-laptop-icon_1076610-101107.jpg" alt="" />
+            </div>
+            <div>
+                <h4>Software Developer</h4>
+                <p>Umair Sajawal</p>
+            </div>
+            </div>
+            
+            <div className="user-card">
+            <div>
+                <img src="https://img.freepik.com/premium-vector/admin-clerk-with-laptop-icon_1076610-101107.jpg" alt="" />
+                </div>
+                <div>
+                <h4>Software Developer</h4>
+                <p>Umair Sajawal</p>
+                </div>
+            </div>
+            
+            <div className="user-card">
+            <div>
+                <img src="https://img.freepik.com/premium-vector/admin-clerk-with-laptop-icon_1076610-101107.jpg" alt="" />
+                </div>
+                <div>
+                <h4>Software Developer</h4>
+                <p>Umair Sajawal</p>
+                </div>
+            </div>
+            <div className="user-card">
+            <div>
+                <img src="https://img.freepik.com/premium-vector/admin-clerk-with-laptop-icon_1076610-101107.jpg" alt="" />
+                </div>
+                <div>
+                <h4>Software Developer</h4>
+                <p>Umair Sajawal</p>
+                </div>
+            </div>
+            <div className="user-card">
+            <div>
+                <img src="https://img.freepik.com/premium-vector/admin-clerk-with-laptop-icon_1076610-101107.jpg" alt="" />
+                </div>
+                <div>
+                <h4>Software Developer</h4>
+                <p>Umair Sajawal</p>
+                </div>
+            </div>
+            <div className="user-card">
+            <div>
+                <img src="https://img.freepik.com/premium-vector/admin-clerk-with-laptop-icon_1076610-101107.jpg" alt="" />
+                </div>
+                <div>
+                <h4>Software Developer</h4>
+                <p>Umair Sajawal</p>
+                </div>
+            </div>
+            <div className="user-card">
+            <div>
+                <img src="https://img.freepik.com/premium-vector/admin-clerk-with-laptop-icon_1076610-101107.jpg" alt="" />
+                </div>
+                <div>
+                <h4>Software Developer</h4>
+                <p>Umair Sajawal</p>
+                </div>
+            </div>
+            <div className="user-card">
+            <div>
+                <img src="https://img.freepik.com/premium-vector/admin-clerk-with-laptop-icon_1076610-101107.jpg" alt="" />
+                </div>
+                <div>
+                <h4>Software Developer</h4>
+                <p>Umair Sajawal</p>
+                </div>
+            </div>
+            <div className="user-card">
+            <div>
+                <img src="https://img.freepik.com/premium-vector/admin-clerk-with-laptop-icon_1076610-101107.jpg" alt="" />
+                </div>
+                <div>
+                <h4>Software Developer</h4>
+                <p>Umair Sajawal</p>
+                </div>
+            </div>
+
+        </div>
+        </>
+    )
+}
+
+export default App
+*/
+
+/*
+Note: First make new folder on "src" named like css and in it make new style file like style.css
+
+style.css file code:
+
+.heading {
+    color: green;
+}
+.container {
+    display: flex;
+    flex-wrap: wrap;
+}
+.user-card {
+    width: 200px; 
+    border: 1px solid #cccccc3b; 
+    border-radius: 5px; 
+    box-shadow: 1px 2px 3px 0px #cccccc57;
+    margin: 10px;
+}
+.user-card img {
+    width: 200px;
+    border-bottom: 1px solid #cccccc57;
+}
+.user-card h4, .user-card p {
+    padding: 5px;
+}
+
+*/
